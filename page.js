7393ce0f9c0f6b2d0437cbab3129010fddd52ef3
Chisom Collins <chizzymomo123@gmail.com>
2024-10-06 16:24:00 +0200

    Update page.js

--- page.js
+++ page.js
@@ -1,4 +1,14 @@
-document.getElementById('download-btn').addEventListener('click', function() {
-    const element = document.body;  
-    html2pdf().from(element).save('MyPortfolio.pdf');
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navbarToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active'); // Toggle the active class to show/hide menu
+});
+
+// Download PDF
+document.getElementById('download-btn').addEventListener('click', () => {
+    const element = document.body; // You can customize what part of the document to include in the PDF
+    html2pdf()
+        .from(element)
+        .save();
 });
